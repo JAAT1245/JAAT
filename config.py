dce9f90fdd81836596f7046e7293099c5b76186c
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-05-04 14:22:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
-# devgagan
-# Note if you are trying to deploy on vps then directly fill values in ("")
+# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
+# Licensed under the GNU General Public License v3.0.  
+# See LICENSE file in the repository root for full license text.
 
-from os import getenv
+import os
+from dotenv import load_dotenv
 
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
+load_dotenv()
+
+# VPS --- FILL COOKIES ðŸª in """ ... """ 
 
 INST_COOKIES = """
 # wtite up here insta cookies
@@ -13,18 +17,18 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
-STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
+API_ID = os.getenv("API_ID", "25255466")
+API_HASH = os.getenv("API_HASH", "aa797f1169fb6bbee3de4869d6b76165")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://daddyfrrr1:aGpA3J5QJqXi5Sq1@cluster0.li1zd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "8080218275").split())) # list seperated via space
+DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
+STRING = os.getenv("STRING", None) # optional
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002650472386")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002182291545")) # optional with -100
+MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
+IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
+INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "10"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
